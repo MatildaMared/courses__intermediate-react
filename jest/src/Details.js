41bc2974fb2798dc5fc9d821d63afbf3cf7137c0
Matilda Mared <matildamared@live.se>
2022-09-20 15:34:50 +0200

    add starter for jest section

--- jest/src/Details.js
+++ jest/src/Details.js
@@ -2,8 +2,8 @@ import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 import Modal from "./Modal";
-import { connect } from "react-redux";
 
 class Details extends Component {
   state = { loading: true, showModal: false };
@@ -31,12 +31,16 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-          <button
-            onClick={this.toggleModal}
-            style={{ backgroundColor: this.props.theme }}
-          >
-            Adopt {name}
-          </button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
           {showModal ? (
             <Modal>
@@ -55,15 +59,11 @@ class Details extends Component {
   }
 }
 
-const mapStateToProps = ({ theme }) => ({ theme });
-
-const ReduxWrappedDetails = connect(mapStateToProps)(Details);
-
 const WrappedDetails = () => {
   const params = useParams();
   return (
     <ErrorBoundary>
-      <ReduxWrappedDetails params={params} />
+      <Details params={params} />
     </ErrorBoundary>
   );
 };
