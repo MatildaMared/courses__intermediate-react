348731c717fad3bcf1182a66d5550885923b6e75
Matilda Mared <matildamared@live.se>
2022-09-20 16:22:01 +0200

    add test for pet component

--- jest/src/__tests__/Pet.test.js
+++ jest/src/__tests__/Pet.test.js
@@ -3,12 +3,17 @@
  */
 
 import { describe, expect, it } from "@jest/globals";
+import { StaticRouter } from "react-router-dom/server";
 import { render } from "@testing-library/react";
 import Pet from "../Pet";
 
 describe("Pet omponent", () => {
   it("displays a default thumbnail", () => {
-    const pet = render(<Pet />);
+    const pet = render(
+      <StaticRouter>
+        <Pet />
+      </StaticRouter>
+    );
     const petThumbnail = pet.getByTestId("thumbnail");
 
     expect(petThumbnail.src).toContain("none.jpg");
