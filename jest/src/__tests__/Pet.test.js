81ef771a0bb2a0eba83d4c2adba17b3a38496c70
Matilda Mared <matildamared@live.se>
2022-09-21 08:24:49 +0200

    add test for pet component

--- jest/src/__tests__/Pet.test.js
+++ jest/src/__tests__/Pet.test.js
@@ -18,4 +18,16 @@ describe("Pet omponent", () => {
 
     expect(petThumbnail.src).toContain("none.jpg");
   });
+
+  it("displays a non-default thumbnail", () => {
+    const pet = render(
+      <StaticRouter>
+        <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
+      </StaticRouter>
+    );
+
+    const petThumbnail = pet.getByTestId("thumbnail");
+
+    expect(petThumbnail.src).toContain("1.jpg");
+  });
 });
